96bbedb6585fc16f1cdd1798eb0bcdf60614ff27
bbkbyti <bbkbyti@gmail.com>
2024-11-22 13:39:56 -0600

    styling buttons using tailwind.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ import NewProject from './components/NewProject';
 
 function App() {
   return (
-    <main className='h-screen my-8'>
+    <main className='h-screen my-8 flex gap-8'>
       <ProjectsSidebar />
       <NewProject />
     </main>
