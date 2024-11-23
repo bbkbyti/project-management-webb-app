8af47ac457b628433bbd320133a86db0101913bc
bbkbyti <bbkbyti@gmail.com>
2024-11-23 16:40:10 -0600

    an error message shows when use leaves the field empty. The error message shows conditionally.

--- my-app/src/components/NewProject.jsx
+++ my-app/src/components/NewProject.jsx
@@ -1,11 +1,16 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Input from './Input'
 
 
 // Next: 
-// add new projects to the side bar and close the form after the new project is added(save button clicked)
+// add some validation for the input
 
 export default function NewProject({ onAdd }) {
+    const [error, setError] = useState(false);
+
+
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -15,6 +20,15 @@ export default function NewProject({ onAdd }) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if (enteredTitle.trim() === ''
+            || enteredDescription.trim() === ''
+            || enteredDueDate.trim() === '') {
+
+            setError(true)
+            return;
+        }
+        setError(false);
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -22,24 +36,27 @@ export default function NewProject({ onAdd }) {
         });
     }
     return (
-        <div className='w-[35rem] mt-16'>
-            <menu className='flex items-center justify-end gap-4 my-4'>
-                <li>
-                    <button
-                        className='text-stone-800 hover:text-stone-950'>Cancel
-                    </button>
-                </li>
-                <li>
-                    <button onClick={handleSave}
-                        className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save
-                    </button>
-                </li>
-            </menu>
-            <div>
-                <Input type='text' ref={title} label="Title" />
-                <Input ref={description} label="Description" textarea />
-                <Input type='date' ref={dueDate} label="Due Date" />
+        <>
+            <div className='w-[35rem] mt-16'>
+                <menu className='flex items-center justify-end gap-4 my-4'>
+                    <li>
+                        <button
+                            className='text-stone-800 hover:text-stone-950'>Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button onClick={handleSave}
+                            className='px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950'>Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type='text' ref={title} label="Title" />
+                    <Input ref={description} label="Description" textarea />
+                    <Input type='date' ref={dueDate} label="Due Date" />
+                    {error && <p className=' flex items-center text-3xl'>You entered Invalid Entry. Please try again!</p>}
+                </div>
             </div>
-        </div>
+        </>
     )
 }
