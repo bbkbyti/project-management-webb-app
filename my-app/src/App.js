9f38e5d40251c81462e70efec5562e0b4c7c6f1d
bbkbyti <bbkbyti@gmail.com>
2024-11-23 13:06:58 -0600

    trying to show the new project form and no project selected component conditionally using useState hook in App.js. When add project button is clicked show the form and if nothing clicked show no project selected modal.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import { useState } from 'react';
 import ProjectsSidebar from './components/ProjectsSidebar';
 import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  const handleStartAddProject = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    });
+  }
   return (
     <main className='h-screen my-8 flex gap-8'>
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <NoProjectSelected onStartAddProject={handleStartAddProject} />
     </main>
   );
 }
