a8587a299d7ba4d3df82d06f67111015c9f67ec7
bbkbyti <bbkbyti@gmail.com>
2024-11-23 13:21:47 -0600

    // Next: // collect values from user when entered into the form and validate them. // show error when invalid input added if valid input entered show it on the side bar. Using useRef hool for this task

--- my-app/src/components/NewProject.jsx
+++ my-app/src/components/NewProject.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { useRef } from 'react'
 import Input from './Input'
 
+
+// Next: 
+// collect values from user when entered into the form and validate them.
+// show error when invalid input added if valid input entered show it on the side bar
+
 export default function NewProject() {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
     return (
         <div className='w-[35rem] mt-16'>
             <menu className='flex items-center justify-end gap-4 my-4'>
