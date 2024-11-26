09a8fff491e76722836a52233af1add915295ccf
bbkbyti <bbkbyti@gmail.com>
2024-11-26 12:58:39 -0600

    Working on Delete button. A project can be deleted using filter method on the project array and the project id. The filter method helps to compare the id of the selected project to the current array of the projects and drop the one with the same id.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -57,9 +57,19 @@ function App() {
   // to check if a new project can be added:
   console.log(projectsState);
 
+  const handleDeleteProject = () => {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
+
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId)
 
-  let content = <SelectedProject project={selectedProject} />
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />
